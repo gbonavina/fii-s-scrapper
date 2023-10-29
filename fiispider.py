00e766ea4c724902f1c03a3b3f73f517b972ecec
gbonavina <132729129+gbonavina@users.noreply.github.com>
2023-10-29 18:59:27 -0300

    Add files via upload

---
+++ fiispider.py
@@ -0,0 +1,17 @@
+import scrapy
+import json
+
+headers = {"X-Funds-Nonce":" 61495f60b533cc40ad822e054998a3190ea9bca0d94791a1da"}
+
+class FiispiderSpider(scrapy.Spider):
+    name = "fiispider"
+    allowed_domains = ["fundsexplorer.com.br"]
+    
+    def start_requests(self):
+        start_urls = ["https://www.fundsexplorer.com.br/wp-json/funds/v1/get-ranking"]
+        for url in start_urls:
+            yield scrapy.Request(url, headers=headers)
+
+    def parse(self, response):
+        for row in json.loads(response.json()):
+            yield row
